0f207cc25cd87031e28c5c3947a48b54361806ef
rebecaCaroba <rebecacaroba@gmail.com>
2023-11-13 09:15:58 -0300

    Estrutura da Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
 import './style.scss'
 
+import camisetas from '../assets/camiseta.png'
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <div className="teste"><button>Home</button></div>
+    <div className='homeContainer'>
+      <a className='product' href="">
+        <Image src={camisetas} alt='camisetas' width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </a>
+      <a className='product' href="">
+        <Image src={camisetas} alt='camisetas' width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </a>
+    </div>
   )
 }
