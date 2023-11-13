90480f44cf967fca8aa3307c1411179928732278
rebecaCaroba <rebecacaroba@gmail.com>
2023-11-13 09:39:17 -0300

    Criando carrossel

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,44 @@
-import './style.scss'
+'use client'
 
+import './style.scss'
+import { useKeenSlider } from 'keen-slider/react'
 import camisetas from '../assets/camiseta.png'
 import Image from 'next/image'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [ sliderRef ]  = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+  
   return (
-    <div className='homeContainer'>
-      <a className='product' href="">
+    <div className='homeContainer keen-slider' ref={sliderRef}>
+      <a className='product keen-slider__slide' href="">
         <Image src={camisetas} alt='camisetas' width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 79,99</span>
         </footer>
       </a>
-      <a className='product' href="">
+      <a className='product keen-slider__slide' href="">
+        <Image src={camisetas} alt='camisetas' width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </a>
+      <a className='product keen-slider__slide' href="">
+        <Image src={camisetas} alt='camisetas' width={520} height={480} />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,99</span>
+        </footer>
+      </a>
+      <a className='product keen-slider__slide' href="">
         <Image src={camisetas} alt='camisetas' width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
