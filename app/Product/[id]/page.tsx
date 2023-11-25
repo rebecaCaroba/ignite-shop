5130b0fd563c994e9cba1367039a7ef828d2ad19
rebecaCaroba <rebecacaroba@gmail.com>
2023-11-25 17:32:11 -0300

    Estrutura da página de produto

--- app/Product/[id]/page.tsx
+++ app/Product/[id]/page.tsx
@@ -1,4 +1,6 @@
-
+import Image from 'next/image'
+import './style.scss'
+import camiseta from '../../../assets/camisaVtex.png'
 interface ProductProps {
   params: {
     id: string
@@ -7,6 +9,20 @@ interface ProductProps {
 
 export default function Productoone({ params }: ProductProps) {
     return (
-      <div>Produtos.. {params.id} </div>
+      <main className="product-container">
+        <div className="img-container">
+        <Image src={camiseta} alt='camisetas' width={520} height={480} />
+        </div>
+        <div className="product-details">
+          <h1>Camiseta x</h1>
+          <span>R$ 99,90</span>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio provident porro ut fugit impedit recusandae asperiores corrupti, libero quibusdam sed cum harum accusantium cumque unde aperiam dolore eaque soluta. Voluptatum?
+          </p>
+          <button>
+            Comprar agora
+          </button>
+        </div>
+      </main>
     )
   }
